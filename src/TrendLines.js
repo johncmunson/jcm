795d29cbdfa48431c55811bc204d5f4bd541c1ff
John Munson <johncmunson91@gmail.com>
2017-03-15 00:27:04 -0500

    add ability to rerender trendlines onclick

--- src/TrendLines.js
+++ src/TrendLines.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import TrendLine from './TrendLine.js'
 
-const TrendLines = (props) => (
-    <div>
-        <TrendLine data={[0, Math.random(), Math.random(), Math.random(), 0]} easing='ease-in' />
-        <TrendLine data={[0, Math.random(), Math.random(), Math.random(), 0]} easing='ease-out' />
-    </div>
-)
+class TrendLines extends React.Component {
+    render() {
+        return (
+            <div onClick={() => this.forceUpdate()}>
+                <TrendLine
+                    data={[0, Math.random(), Math.random(), Math.random(), 0]}
+                    easing='ease-in'
+                />
+                <TrendLine
+                    data={[0, Math.random(), Math.random(), Math.random(), 0]}
+                    easing='ease-out'
+                />
+            </div>
+        );
+    }
+}
 
 export default TrendLines
