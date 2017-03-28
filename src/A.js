df59a2ce6fb7ce7126b4aeeaff739f8a39d2886f
John Munson <johncmunson91@gmail.com>
2017-03-28 12:26:07 -0600

    adjust trendlines

--- src/A.js
+++ src/A.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 const A = styled.a`
     color: #16BFFD;
     text-decoration: none;
+    cursor: crosshair;
     &:hover {
         text-decoration: underline;
     }
--- src/TrendLines.js
+++ src/TrendLines.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import TrendLine from './TrendLine.js'
 
+function getRand(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 class TrendLines extends React.Component {
     render() {
         return (
             <div onClick={() => this.forceUpdate()}>
                 <TrendLine
-                    data={[0, Math.random(), Math.random(), Math.random(), 0]}
+                    data={[0, Math.random() + 0.5, getRand(0.4, 0.8), Math.random() + 0.5, 0]}
                     easing='ease-in'
                 />
                 <TrendLine
-                    data={[0, Math.random(), Math.random(), Math.random(), 0]}
+                    data={[0, Math.random() + 0.5, getRand(0.4, 0.8), Math.random() + 0.5, 0]}
                     easing='ease-out'
                 />
             </div>
