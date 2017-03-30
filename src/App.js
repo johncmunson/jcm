31c2f5cee2194367e7c395bc4d8c260d4229b264
John Munson <johncmunson91@gmail.com>
2017-03-30 01:56:22 -0600

    add bitmoji's and demonstrate fixed positioning

--- src/App.js
+++ src/App.js
@@ -5,11 +5,27 @@ import Footer from './Footer.js'
 import A from './A.js'
 import styled from 'styled-components'
 import './App.css'
+import office from './media/office.png'
+import thuglife from './media/thuglife.png'
 
 const StickyFooterWrapper = styled.div`
     display: flex;
     flex-direction: column;
     min-height: 100vh;
+`
+
+const Img1 = styled.img`
+    position: fixed;
+    bottom: 0;
+    left: 30px;
+    height: 115px;
+    width: 100px;
+`
+
+const Img2 = styled.img`
+    position: fixed;
+    bottom: 0;
+    right: 0;
 `
 
 const App = (props) => (
@@ -23,6 +39,8 @@ const App = (props) => (
             <br />
             Fork me on <A href='https://github.com/johncmunson/jcm'><u>Github.</u></A>
         </Footer>
+        <Img1 className='animated fadeIn' src={thuglife} />
+        <Img2 className='animated fadeIn' src={office} />
     </StickyFooterWrapper>
 )
 
